fe7559238b1e6b7d4a2555ad8fdc3c900251a5f6
pulicapus <pulicapus@yandex.ua>
2016-10-06 18:19:05 +0300

    init

---
+++ includes/functions.php
@@ -0,0 +1,45 @@
+<?php
+
+if ( ! function_exists( 'tm_wc_ajax_filters_after_subcategory' ) ) {
+
+	function tm_wc_ajax_filters_after_subcategory( $category, $atts = '' ) {
+
+		if ( 'list' === $atts ) {
+
+			echo apply_filters( 'tm_woo_grid_list_cat_desc_on_list', '<div class="product-category__description">' . $category->category_description . '</div>', $category );
+		}
+	}
+}
+
+if ( ! function_exists( 'tm_wc_ajax_filters_shop_loop_subcategory_title' ) ) {
+
+	function tm_wc_ajax_filters_shop_loop_subcategory_title( $category, $atts = '' ) {
+
+		if ( 'list' === $atts ) { ?>
+		<h3>
+			<?php
+				echo $category->name;
+			?>
+		</h3>
+		<?php if ( $category->count > 0 ) {
+
+			global $wp_query;
+
+			if ( 1 == $category->count ) {
+
+				$name = isset( $wp_query->queried_object->labels->singular_name ) ? $wp_query->queried_object->labels->singular_name : __( 'Product', 'tm-woocommerce-package' );
+			} else {
+
+				$name = isset( $wp_query->queried_object->labels->name ) ? $wp_query->queried_object->labels->name : __( 'Products', 'tm-woocommerce-package' );
+			}
+
+			echo apply_filters( 'woocommerce_subcategory_count_html_list', ' <div class="count"><span>' . $category->count . '</span> ' . $name . '</div>', $category );
+			}
+		} else {
+
+			woocommerce_template_loop_category_title( $category );
+		}
+	}
+}
+
+?>
